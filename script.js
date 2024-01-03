d2b8bc6e80ad768fbf23e73f128acf19d791a4dd
therealkingcob <rishiashah19@gmail.com>
2024-01-02 16:29:51 -0800

    finished computer choice

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ let winner = "";
 let playerWins = 0;
 let computerWins = 0;
 
+
+//return a string that is the computers choice
 function getComputerChoice() {
     let random_number = randomNumber();
-    let computerChoice = ""
+    let computerChoice = "";
 
-    if (random_number === 1) {
+    if (random_number === 0     ) {
         computerChoice = "paper";
-    } else if (random_number === 2) {
+    } else if (random_number === 1) {
         computerChoice = "scissor";
-    } else if (random_number === 3) {
+    } else if (random_number === 2) {
         computerChoice = "rock";
     }
 
     return computerChoice;
 }
+
+function randomNumber() {
+    return Math.floor(Math.random() * 3); //randomly return 0, 1, 2
+}
+
